7e10327c6339753b7c7c6bb7054ac1c81b8f4e9b
zianni02 <66231450+zianni02@users.noreply.github.com>
2020-06-26 03:20:52 -0500

    Add files via upload

---
+++ Laboratorio 6/app.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const connectDB = require('./dbConfig');
+const Estudiantes = require('./models/Estudiantes');
+
+const PORT = 3000;
+
+
+app.use(bodyParser.json());
+
+app.get('/api/estudiantes/', async (req, res) => {
+    const estudiantes = await Estudiantes.find().select('nombre edad');
+  res.json({
+        estudiantes,
+        cantidad: estudiantes.length
+    });
+});
+
+app.post('/api/estudiantes/', async (req, res) => {
+    const { nombre, edad } = req.body;
+    await Estudiantes.create({ nombre, edad});
+    res.json({ nombre, edad});
+});
+
+app.get('/api/estudiantes/:id', async (req, res) => {
+    try{
+        const estudiante = await Estudiantes.findById(req.params.id).select('nombre edad');
+        res.json(estudiante);
+    } catch (error){
+        console.log(error);
+        res.json({});
+    }
+});
+
+app.put('/api/estudiantes/:id', async (req, res) => {
+    const { nombre, edad }= req.body;
+    await Estudiantes.update({nombre, edad});
+   return res.send(`La lista fue modificada`);
+});
+
+app.delete('/api/estudiantes/:id', async (req, res) =>{
+    await Estudiantes.findOneAndDelete(req.params.id);
+return res.send (`fue borrado el estudiante`);
+});
+
+
+connectDB().then (() => {
+    app.listen(PORT, () => {
+        console.log(`Ejecutando en el puerto ${PORT}`);
+    });
+});
---
+++ Laboratorio 6/moduls/Estudiantes.js
@@ -0,0 +1,11 @@
+const mongoose = require ('mongoose');
+
+
+const schema = new mongoose.Schema({
+    nombre: { type: String },
+    edad: {type: String }
+});
+
+const Estudiantes = mongoose.model(`Estudiantes`, schema);
+
+module.exports = Estudiantes;
